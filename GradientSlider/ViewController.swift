052da09a57bec16c09ec40accd4b474440663b6a
Jonathan Hull <jhull@gbis.com>
2015-08-05 20:21:39 -0700

    Initial Commit

---
+++ GradientSlider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GradientSlider
+//
+//  Created by Jonathan Hull on 8/5/15.
+//  Copyright © 2015 Jonathan Hull. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
